b9cd1fe955e1aa0f1f3cd5afdfc2514a1d543d85
krishnanjalivu <vukrishnanjali@gmail.com>
2024-05-18 02:37:19 +0530

    auth login backend

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import "dotenv/config"
 import mongoose from 'mongoose'
 import userRoutes from './routes/users'
+import authRoutes from './routes/auth'
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
@@ -13,7 +14,7 @@ app.use(cors())
 // app.get("/api/test",async(req:Request,res:Response)=>{
 //   res.json({message:"hello from express endpoint"})
 // })
-
+app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 
 app.listen(7000,()=>{
